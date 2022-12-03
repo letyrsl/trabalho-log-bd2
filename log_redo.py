94315d823f02d7ceea3aebec723c882068815936
Évellyn Camatti <evellyncco@gmail.com>
2022-12-03 14:59:46 -0300

    Ajustando funções

--- log_redo.py
+++ log_redo.py
@@ -1,33 +1,49 @@
 # Biblioteca do regEx 
 import re
 
+def find_checkpoint(file):
+  # Encontrando o checkpoint e salvando as transações que ainda não terminaram
+  transactions = []
+
+  for line in reversed( list(file) ):
+    matches = re.search('<CKPT \((.+?)\)>', line)
+    if matches:
+      transactions.append(matches.group(1).split(","))
+      break
+
+  # Retona pra início do arquivo
+  file.seek(0)
+
+  return transactions
+
+def find_committed_transations(file):
+  # Encontrando quais transações finalizaram depois do checkpoint  
+  transactions = []
+
+  for line in reversed( list(file) ):
+    # Só vai percorrer até encontrar um checkpoint
+    if ("CKPT" in line): break
+
+    matches = re.search('<commit (.+?)>', line)
+    if matches:
+      transactions.append(matches.group(1))
+
+  # Retona pra início do arquivo
+  file.seek(0)
+
+  return transactions
+
 def log_redo(cursor):
   # Abre arquivo da entradaLog apenas para leitura
   file = open('test_files/entradaLog', 'r')
 
   try:
-    # Encontrando o checkpoint e salvando as transações que ainda não terminaram
-    checkpoint_transactions = []
-    for line in reversed( list(file) ):
-      matches = re.search('<CKPT \((.+?)\)>', line)
-      if matches:
-        transactions = matches.group(1)
-        checkpoint_transactions.append(transactions.split(","))
-        break
-    
-    # Retonar pra início do arquivo
-    file.seek(0)
+    checkpoint_transactions = find_checkpoint(file)
 
-    # Encontrando quais transações finalizaram depois do checkpoint  
-    committed_transactions = []
-    for line in reversed( list(file) ):
-      matches = re.search('<commit (.+?)>', line)
-      if matches:
-        transactions = matches.group(1)
-        committed_transactions.append(transactions.split(","))
+    committed_transactions = find_committed_transations(file)
 
-    print(committed_transactions)
-      
+    print(checkpoint_transactions, committed_transactions)
+
   finally:
     # Fecha arquivo
     file.close()
